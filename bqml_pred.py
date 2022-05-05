7c064c67fc7141938a35e167d777f434e419eaff
Akira Noda <akiranoda85@gmail.com>
2022-05-05 01:00:09 +0000

    Added BigQueryML stuff

---
+++ bqml_pred.py
@@ -0,0 +1,26 @@
+#Module3
+
+import os
+
+from flask import Flask, request, jsonify
+from google.cloud import bigquery
+
+client = bigquery.Client()
+
+query_job = client.query(
+    """
+    SELECT * 
+    FROM `msds434-project.ZenDesk.model_eval`
+    """
+)
+eval_results = query_job.result()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+
+    return jsonify(eval_results)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
