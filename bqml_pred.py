363ac35c7316da436ddd52d4ec7572be92492434
Akira Noda <akiranoda85@gmail.com>
2022-05-05 05:02:09 +0000

    update

--- bqml_pred.py
+++ bqml_pred.py
@@ -1,28 +1,24 @@
 #Module5
 import os
 import pandas
-import pyarrow
 from flask import Flask, request, jsonify
 from google.cloud import bigquery
 
-app = Flask(__name__)
-@app.route('/')
-
-
 client = bigquery.Client()
-
 table = bigquery.TableReference.from_string(
     "msds343-project.ZenDesk.model_eval"
 )
 rows = client.list_rows(
     table,
   )
-
 dataframe = rows.to_dataframe()
+
+app = Flask(__name__)
+@app.route('/')
 
 def hello():
 
     print(dataframe)
 
-if __name__ == "__main__":
+if __name__ == "__bqml_pred__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
