c9229c091fd45beab9a79b328f4bcb2c80bbb1b9
Akira Noda <akiranoda85@gmail.com>
2022-05-05 03:29:12 +0000

    bigQ

--- bqml_pred.py
+++ bqml_pred.py
@@ -1,26 +1,31 @@
-#Module3
-
+#Module5
 import os
-
+import pandas as pd
 from flask import Flask, request, jsonify
 from google.cloud import bigquery
 
+app = Flask(__name__)
+@app.route('/')
+
+
 client = bigquery.Client()
 
-query_job = client.query(
-    """
-    SELECT * 
-    FROM `msds343-project.ZenDesk.model_eval`
-    """
+query_string = """
+SELECT * 
+FROM `msds343-project.ZenDesk.model_eval`
+"""
+
+dataframe = (
+    client.query(query_string)
+    .result()
+    .to_dataframe(
+        create_bqstorage_client=True,
+    )
 )
-eval_results = query_job.result()
 
-app = Flask(__name__)
-
-@app.route('/')
 def hello():
 
-    return jsonify(eval_results)
+    print(dataframe)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
