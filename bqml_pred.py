35d81ee622b8bc0cebcde621075b851125d7b2b7
Akira Noda <akiranoda85@gmail.com>
2022-05-30 19:13:37 +0000

    fix upload

--- bqml_pred.py
+++ bqml_pred.py
@@ -1,9 +1,9 @@
-#Module5
 import os
 from os.path import join, dirname, realpath
 import pandas
 from flask import Flask, request, jsonify, make_response, render_template_string, render_template, url_for, redirect
 from google.cloud import bigquery
+from werkzeug.utils import secure_filename
 
 client = bigquery.Client()
 table = bigquery.TableReference.from_string(
@@ -23,37 +23,37 @@ job_config.source_format = bigquery.SourceFormat.CSV
 job_config.autodetect = True
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'static/files'
+path = os.getcwd()
+UPLOAD_FOLDER = os.path.join(path, 'uploads')
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-
 @app.route('/')
-def index():
+def upload():
     return render_template('upload.html')
 
 @app.route('/', methods=['POST'])
 def uploadFiles():
     uploaded_file = request.files['file']
     if uploaded_file.filename !='':
-        #file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-        #uploaded_file.save(uploaded_file.filename)
-        with open(uploaded_file, "rb") as source_file:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        uploaded_file.save(file_path)
+        with open(file_path, "rb") as source_file:
             job = client.load_table_from_file(source_file, temp_table, job_config=job_config)
         job.result()
-        print("Loaded {} rows into {}:{}.")
 
     return redirect(url_for('index'))
 
-def main():
-    return "Akira Noda - MSDS434 Project - Customer Support Ticket Type Prediction"
 
-def hello():
 
-    df_html = dataframe.describe().to_html()
-    resp = make_response(render_template_string(df_html))
+#def hello():
 
-    return resp
+#    df_html = dataframe.describe().to_html()
+#    resp = make_response(render_template_string(df_html))
+
+#    return resp
 
 if __name__ == "__bqml_pred__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
