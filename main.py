cc5f1dc722e09d7f64d1fe4d95d9d2b280ca5da6
Akira Noda <103208143+Akimon85@users.noreply.github.com>
2022-06-04 05:58:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -328,7 +328,7 @@ def update_figure(X):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=8080)
+    app.run_server(debug=True, host='0.0.0.0', port=8080)
 
 
 #if __name__ == "__main__":
