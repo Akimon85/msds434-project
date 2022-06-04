cee42357449812bb54e2c4c7f0dfe1c26c6a9f46
Akira Noda <103208143+Akimon85@users.noreply.github.com>
2022-06-04 12:23:15 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+
+    return jsonify({'output':'Hello World! (I hope this works)'})
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
