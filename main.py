0218b8365fa68d10a9ddbf8009b7f63c4753ae96
Akira Noda <103208143+Akimon85@users.noreply.github.com>
2022-06-04 20:34:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -261,7 +261,7 @@ app.layout = html.Div(
                     html.Label("Choose a feature"),
                     dcc.Dropdown(id = 'opt',
                                  options = opts,
-                                 value = opts[0][value])
+                                 value = opts[0])
                     ], style = {'width': '400px',
                                 'fontSize' : '20px',
                                 'padding-left' : '100px',
